6bc2baa5825cdcd95173f2e967884e13a538f237
thiagocifani <cifani.thiago@gmail.com>
2025-03-06 23:57:26 -0300

    feat: add updates

--- nvim/lua/thiagocifani/plugins/formatting.lua
+++ nvim/lua/thiagocifani/plugins/formatting.lua
@@ -20,6 +20,7 @@ return {
 				liquid = { "prettier" },
 				lua = { "stylua" },
 				ruby = { "standardrb" },
+				erb = { "erb_format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
--- nvim/lua/thiagocifani/plugins/telescope.lua
+++ nvim/lua/thiagocifani/plugins/telescope.lua
@@ -29,7 +29,12 @@ return {
 		--set keymaps
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+		keymap.set(
+			"n",
+			"<leader>ff",
+			"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+			{ desc = "Fyzzy find files in cwd" }
+		)
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Live grep with telescope" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursos in cwd" })
