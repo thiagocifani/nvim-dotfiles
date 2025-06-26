70331e153f1c02fc4802bcd60afb1603ec256300
thiagocifani <cifani.thiago@gmail.com>
2025-06-26 01:34:43 +0100

    feat: ensure node copilot is working as required

--- nvim/lua/thiagocifani/plugins/copilot.lua
+++ nvim/lua/thiagocifani/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
 	"github/copilot.vim",
 	config = function()
+		vim.g.copilot_node_command = vim.fn.expand("~/.asdf/installs/nodejs/24.1.0/bin/node")
 		vim.cmd([[highlight CopilotSuggestion ctermfg=8 guifg=white guibg=#5c6370]])
 	end,
 }
